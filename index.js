09db116d2c06e327e9cc56513dadeea7e88d2df8
George Harutyunyan <>
2019-10-19 15:52:14 -0400

    part 3.3 complete

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
 const express = require('express')
 const app = express()
-const PORT = 3001
+const PORT = 3005
 
 const bodyParser = require('body-parser')
 app.use(bodyParser.json())
 
 
-const notes = [{
-    name:'George',
-    number:'611'
-}]
+const notes = [
+  {
+    name:"George",
+    number: "611",
+    id:1
+  },
+  {
+    name: "Anna",
+    number: "1234",
+    id:2
+  },
+  {
+    name: "Gohar",
+    number: "1111",
+    id:3
+  }
+]
 
 app.get('/api/persons',(request,response) => response.json(notes))
 
@@ -18,6 +31,15 @@ app.get('/info', (request, response) => {
     response.write(`Phonebook has info for ${notes.length} people\n`)
     response.write(new Date().toString())
     response.end()
+})
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = notes.find(n => n.id === id)
+    if (person) {
+        response.json(person)
+    }
+    response.status(404).end()
 })
 
 
